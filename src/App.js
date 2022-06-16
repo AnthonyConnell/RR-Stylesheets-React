8687c2e4252d9689b9d57661ce75d5d16a24a8a4
Anthony Connell <toco85@gmail.com>
2022-06-15 21:32:59 -0400

    Added one style, need 2 more

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import React from 'react';
+
+const styledText = {
+  fontSize: "25px",
+  textAlign: "center",
+  margin: "40px",
+  border: "1px solid green"
+};
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={styledText}>
       <h1>Hello World</h1>
     </div>
   );
